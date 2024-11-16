23999094be7d0e82d9d172bdb3ffbb00788af9d4
SMauhourat <santiagomauhourat@hotmail.com>
2024-11-16 13:11:58 -0300

    varios

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,33 +1,6 @@
-import { authenticate } from "@/services/authService"
 import NextAuth from "next-auth"
-import { AuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import { options } from "./options"
 
-export const authOptions {
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials, req) {
-                if (typeof credentials !== "undefined") {
-                    const res = await authenticate(credentials.email, credentials.password)
-                    if (typeof res !== "undefined") {
-                        return { ...res.user, apiToken: res.token }
-                    } else {
-                        return null
-                    }
-                } else {
-                    return null
-                }
-            }
-        })
-    ],
-session: { strategy: "jwt" }
-}
-
-const handler = NextAuth(authOptions)
+const handler = NextAuth(options)
 
 export { handler as GET, handler as POST }
