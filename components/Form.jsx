fe371ccc67f5536b3d7d36972b7bc4881daf1bec
SMauhourat <santiagomauhourat@hotmail.com>
2024-11-20 09:24:23 -0300

    form2

--- components/Form.jsx
+++ components/Form.jsx
@@ -3,11 +3,24 @@ import React from 'react'
 const Form = ({ type, post, setPost, handleSubmit }) => {
     return (
         <section className='w-full max-w-full flex-start flex-col'>
-            <textarea 
-                className='form_textarea'
-                value={post.prompt}
+            <h1 className='head_text text-left'>
+                <span className='blue_gradient'>{type} Post</span>
+            </h1>
+            <p className='desc text-left max-w-md'>
+                {type} and share amazing prompts with the world, and let your
+                imagination run wild with any AI-powered platform
+            </p>
+            <form
+                onSubmit={handleSubmit}
+                className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
             >
-            </textarea>
+                <textarea 
+                    className='form_textarea'
+                    value={post.prompt}
+                >
+                </textarea>
+
+            </form>
         </section>
     )
 }
